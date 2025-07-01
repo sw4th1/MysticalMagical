033ec368c9fcd23b766e86b7202a1d436fcd4c04
Jaden Ambrocio Alcantara <73153822+jaiecodes@users.noreply.github.com>
2025-07-01 16:30:00 -0500

    initial project template

---
+++ llm-music/src/run_query.py
@@ -0,0 +1,14 @@
+from haystack_pipeline import build_pipeline
+
+def run_query_loop(initial_prompt):
+    pipeline = build_pipeline()
+
+    result = pipeline.run(query=initial_prompt, params={"Retriever": {"top_k": 4}})
+    docs = result["documents"]
+
+    ret_tracks = []
+    for doc in docs:
+        ret_tracks.append(doc.meta["uri"])
+    
+    return ret_tracks
+          
