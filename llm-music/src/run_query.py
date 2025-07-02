8e149af66384cd1f6748746682448fa7493a8eb9
Jaden Ambrocio Alcantara <73153822+jaiecodes@users.noreply.github.com>
2025-07-01 21:35:20 -0500

    webserver code

--- llm-music/src/run_query.py
+++ llm-music/src/run_query.py
@@ -3,7 +3,7 @@ from src.haystack_pipeline import build_pipeline
 def run_query_loop(initial_prompt):
     pipeline = build_pipeline()
 
-    result = pipeline.run(query=initial_prompt, params={"Retriever": {"top_k": 4}})
+    result = pipeline.run(query=initial_prompt, params={"Retriever": {"top_k": 10}})
     docs = result["documents"]
 
     ret_tracks = []
