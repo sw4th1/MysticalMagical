46396cf4586300839a2ae328fe72a9b8f1701675
Gerardo Carmona <gerardo.carmonaruiz@brunswick.com>
2025-07-01 19:00:34 -0500

    Changed the look of the UI

--- llm-music/src/main_app.py
+++ llm-music/src/main_app.py
@@ -1,6 +1,9 @@
 import flet as ft
 import requests
 
+# from flet.border import BorderSide
+# from flet.buttons import RoundedRectangleBorder
+
 
 # The URL endpoint to send the POST request to
 url = "http://127.0.0.1:5000/receive_data"
@@ -11,8 +14,34 @@ payload_json = {
     "timer": "5"
 }
 
+height_width = 500  # Default size for the window and controls
 
 def main(page: ft.Page):
+    page.always_on_top = True  # Keep the window always on top
+    page.window.width = height_width
+    page.window.height = 450
+    page.window.resizable = False
+    page.window.maximizable = False
+    page.title_bar_hidden = True  # Hide the title bar
+    page.window.center()  # Center the window on the screen
+    
+    #chanmge background color to black
+    page.bgcolor = ft.Colors.BLACK
+
+    page.theme = ft.Theme(
+        slider_theme=ft.SliderTheme(
+
+            value_indicator_text_style=ft.TextStyle(
+                color=ft.Colors.WHITE,  # Set the text color of the value indicator
+            )
+        )
+    )
+
+
+
+    page.update()
+
+
     prompt_text = []
     mix_timer = 5  # default value
 
@@ -38,14 +67,15 @@ def main(page: ft.Page):
 
 
 
-
-
     def on_slider_change(e):
         nonlocal mix_timer
         mix_timer = int(e.control.value)
         # Optionally, update a label or print for debugging
         print("mix_timer:", mix_timer)
 
+    def no_action(e):
+        # This function does nothing, can be used for testing purposes
+        print("No action performed.")
 
     def send_values(e):
         # This function can be used to send the values to a server or process them
@@ -66,38 +96,56 @@ def main(page: ft.Page):
 
 
     text_Title = ft.Text(
-        "MixSpace / MixStudio",
+        "MixSpace",
         size=20,
         weight="bold",
         text_align="center",
     )
 
+    img_slider = ft.Image(
+        src=f"Images/Sliders.png",
+        width=100,
+        height=100,
+        fit=ft.ImageFit.CONTAIN,
+    )
+    
+
+
     text_filterinput = ft.TextField(
         label="Add filters",
         on_change=on_text_change,
-        width=300,
+        width=height_width,
         expand=True,
     )
 
     slider_timer = ft.Slider(
-        min=5,
-        max=20,
-        divisions=15,
-        value=5,
-        label="{value}",
-        on_change=on_slider_change,
-        width=300,
+            min=5,
+            max=20,
+            divisions=15,
+            value=5,
+            label="{value}",
+            on_change=on_slider_change,
+            
+            
+            thumb_color=ft.Colors.GREEN,
+            active_color=ft.Colors.GREEN_900,
+            inactive_color=ft.Colors.GREEN_200,
+            
+            width=height_width, 
     )
 
+
+
     view_title = ft.Column(
-        width=300,
+        width=height_width,
+        horizontal_alignment=ft.CrossAxisAlignment.CENTER,
         controls=[
             text_Title,
         ]
     ) 
 
     view_filters = ft.Column(
-        width=300,
+        width=height_width,
         controls=[
             ft.Row(
                 controls=[
@@ -110,7 +158,7 @@ def main(page: ft.Page):
     ) 
 
     view_timer = ft.Column(
-        width=300,
+        width=height_width,
         controls=[
             ft.Text("Shuffle Timer:"),
             slider_timer,
@@ -123,21 +171,49 @@ def main(page: ft.Page):
             view_title,
             view_filters,
             view_timer,
-            ft.ElevatedButton(
-                text="Send Values",
-                on_click=send_values,
-                width=300,
-                expand=True,
+            ft.Row(
+                alignment=ft.MainAxisAlignment.CENTER,
+                # horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                width=height_width,
+                controls=[
+                    ft.ElevatedButton(
+                        content=ft.Image("Images/music_disc.gif"),
+                        text="Send Values",
+                        on_click=no_action,
+                        width=150,
+                        expand=True,
+                        # style=ft.ButtonStyle(
+                        #     side={
+                        #         ft.ControlState.HOVERED: ft.BorderSide(0, ft.Colors.GREEN_900), # 2 pixels wide, green color
+                        #     }
+                        # ),
+                        bgcolor=ft.Colors.TRANSPARENT,
+
+                    ),
+                    # insert image here 
+                    img_slider,
+                    
+                    ft.ElevatedButton(
+                        content=ft.Image("Images/music_disc.gif"),
+                        text="Send Values",
+                        on_click=send_values,
+                        width=150,
+                        expand=True,
+                        bgcolor=ft.Colors.TRANSPARENT,
+                    ),
+                ]
             ),
+
+            
+            
             
         ])
     )
 
 
-    page.title = "MixSpace / MixStudio"
+    page.title = "MixSpace"
     page.vertical_alignment = ft.MainAxisAlignment.START
     page.theme_mode = ft.ThemeMode.DARK
-    page.window_width = 400
-    page.window_height = 200
+    
 
 ft.app(target=main)
